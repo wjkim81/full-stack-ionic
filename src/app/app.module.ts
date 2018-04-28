5348070aee3dba4e59e6dd6e3d1b11516b93541c
wjkim81 <onejean81@gmail.com>
2018-04-29 02:42:59 +0900

    Getting Started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,10 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { AboutPage } from '../pages/about/about';
+import { MenuPage } from '../pages/menu/menu';
+import { ContactPage } from '../pages/contact/contact';
+
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -13,7 +16,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    AboutPage,
+    MenuPage,
+    ContactPage
   ],
   imports: [
     BrowserModule,
@@ -23,7 +28,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    AboutPage,
+    MenuPage,
+    ContactPage
   ],
   providers: [
     StatusBar,
