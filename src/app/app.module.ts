6b4de42f0eaee62e41e1bb56b479c5417d8dc335
wjkim81 <onejean81@gmail.com>
2018-05-01 04:22:57 +0900

    Ionic Navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomePage } from '../pages/home/home';
 import { AboutPage } from '../pages/about/about';
 import { MenuPage } from '../pages/menu/menu';
 import { ContactPage } from '../pages/contact/contact';
+import { DishdetailPage } from '../pages/dishdetail/dishdetail';
 
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -24,7 +25,8 @@ import { baseURL } from '../shared/baseurl';
     HomePage,
     AboutPage,
     MenuPage,
-    ContactPage
+    ContactPage,
+    DishdetailPage
   ],
   imports: [
     BrowserModule,
@@ -37,7 +39,8 @@ import { baseURL } from '../shared/baseurl';
     HomePage,
     AboutPage,
     MenuPage,
-    ContactPage
+    ContactPage,
+    DishdetailPage
   ],
   providers: [
     StatusBar,
--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+import { Dish } from '../../shared/dish';
+import { DishProvider } from '../../providers/dish/dish';
+import { DishdetailPage } from '../dishdetail/dishdetail';
 
 /**
  * Generated class for the MenuPage page.
@@ -13,13 +17,32 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   selector: 'page-menu',
   templateUrl: 'menu.html',
 })
-export class MenuPage {
+export class MenuPage implements OnInit {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  dishes: Dish[];
+  errMess: string;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private dishservice: DishProvider,
+    @Inject('BaseURL') private BaseURL) {
+  }
+
+  ngOnInit() {
+    this.dishservice.getDishes()
+      .subscribe(dishes => this.dishes = dishes,
+        errmess => this.errMess = <any>errmess);
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MenuPage');
   }
 
+  dishSelected(event, dish) {
+    // That's right, we're pushing to ourselves!
+    this.navCtrl.push(DishdetailPage, {
+      dish: dish
+    });
+  }
 }
